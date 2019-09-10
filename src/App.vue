ae3821ec6e8d1ab24d561b4f809da62afac86ca4
VitakSharper <v.banard@gmail.com>
2019-09-10 15:56:48 +0200

    add server directory Node.Js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="app">
-        <app-home/>
-        <app-navbar/>
-        <app-footer/>
-    </div>
+  <div id="app">
+    <app-navbar/>
+    <app-home/>
+    <app-footer/>
+  </div>
 </template>
 <script>
     import Home from "./views/Home";
@@ -20,40 +20,40 @@
 </script>
 
 <style lang="scss">
-    @import "~bulma/bulma.sass";
-    @import "assets/spacing.css";
+  @import "~bulma/bulma.sass";
+  @import "assets/spacing.css";
 
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-    .bold {
-        font-weight: bold;
-    }
+  .bold {
+    font-weight: bold;
+  }
 
-    .cover {
-        background-size: cover;
-        /*background-position: center;*/
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
+  .cover {
+    background-size: cover;
+    /*background-position: center;*/
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+  }
 
-    .hero {
-        position: relative;
-    }
+  .hero {
+    position: relative;
+  }
 
-    .hero-body {
-        padding: 3rem 1.5rem;
-    }
+  .hero-body {
+    padding: 3rem 1.5rem;
+  }
 
-    .hero-bg {
-        background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('https://images.unsplash.com/photo-1531263060782-b024de9b9793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
+  .hero-bg {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('https://images.unsplash.com/photo-1531263060782-b024de9b9793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+  }
 </style>
