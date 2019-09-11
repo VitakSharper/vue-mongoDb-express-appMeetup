e071a51ccf8243aeb0deabdf40e87787d7e41bba
VitakSharper <v.banard@gmail.com>
2019-09-11 20:50:45 +0200

    seeding database

--- server/index.js
+++ server/index.js
@@ -15,10 +15,6 @@ const meetupsRoutes = require('./routes/meetups'),
     postsRoutes = require('./routes/posts'),
     categoriesRoutes = require('./routes/categories');
 
-mongoose.connect(config.DB_URI, {useNewUrlParser: true})
-    .then(() => console.log('DB Connected!'))
-    .catch(err => console.log(err));
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -31,6 +27,23 @@ app.use('/api/v1/categories', categoriesRoutes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function () {
-    console.log('App is running on port: ' + PORT);
-});
+
+async function start() {
+    try {
+        if (await mongoose.connect(config.DB_URI, {
+            useNewUrlParser: true, useFindAndModify: false
+        })) {
+            console.log('DB Connected');
+            app.listen(PORT, () => {
+                console.log('App is running on port: ' + PORT);
+            });
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start();
+
+
+
--- server/seedDB.js
+++ server/seedDB.js
@@ -9,57 +9,77 @@ const data = require('./data.js');
 const config = require('./config/dev');
 
 class DB {
-  constructor() {
-    this.meetups = data.meetups;
-    this.users = data.users;
-    this.threads = data.threads;
-    this.posts = data.posts;
-    this.categories = data.categories;
-    this.models = [Meetup, User, Post, Thread, Category];
-  }
-
-  async cleanDb() {
-     for ( let model of this.models ) {
-      await model.deleteMany({}, () => {})
-      console.log(`Data for model ${model.collection.collectionName} Deleted!`)
+    constructor() {
+        this.meetups = data.meetups;
+        this.users = data.users;
+        this.threads = data.threads;
+        this.posts = data.posts;
+        this.categories = data.categories;
+        this.models = [Meetup, User, Post, Thread, Category];
     }
-  }
 
-  async pushDataToDb() {
-    await this.categories.forEach(async (category) => {
-       const newCategory = new Category(category);
-       await newCategory.save(() => {});
-    })
+    async cleanDb() {
+        for (let model of this.models) {
+            await model.deleteMany({}, () => {
+            });
+            console.log(`Data for model ${model.collection.collectionName} Deleted!`)
+        }
+    }
 
-    await this.users.forEach(async user => {
-      await (new User(user)).save(() => {})
-    })
+    async pushDataToDb() {
+        await this.categories.map(async (category) => {
+            const newCategory = new Category(category);
+            await newCategory.save(() => {
+            });
+        });
 
-    await this.meetups.forEach(async meetup => {
-      await (new Meetup(meetup)).save(() => {})
-    })
+        await this.users.map(async user => {
+            await (new User(user)).save(() => {
+            })
+        });
 
-    await this.threads.forEach(async thread => {
-      await (new Thread(thread)).save(() => {})
-    })
+        await this.meetups.map(async meetup => {
+            await (new Meetup(meetup)).save(() => {
+            })
+        });
 
-    await this.posts.forEach(async post => {
-      await (new Post(post)).save(() => {})
-    })
+        await this.threads.map(async thread => {
+            await (new Thread(thread)).save(() => {
+            })
+        });
 
-    console.log('Database Populated!');
-  }
+        await this.posts.map(async post => {
+            await (new Post(post)).save(() => {
+            })
+        });
 
-  async seedDb() {
-    await this.cleanDb();
-    await this.pushDataToDb();
-  }
+        console.log('Database Populated!');
+    }
+
+    async seedDb() {
+        await this.cleanDb();
+        await this.pushDataToDb();
+    }
 }
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true })
-  .then(async () => {
-    const db = new DB();
-    await db.seedDb();
-    console.log('You can close connection now!')
-  })
-  .catch(err => console.log(err));
+async function startSeed() {
+    try {
+        if (await mongoose.connect(config.DB_URI, {useNewUrlParser: true})) {
+            const db = new DB();
+            await db.seedDb();
+            console.log('You can close connection now!')
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+startSeed();
+
+// mongoose.connect(config.DB_URI, {useNewUrlParser: true})
+//     .then(async () => {
+//         const db = new DB();
+//         await db.seedDb();
+//         console.log('You can close connection now!')
+//     })
+//     .catch(err => console.log(err));
