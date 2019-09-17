b29d4b0583af3633c44b8fa8c836844c63476dd4
VitakSharper <v.banard@gmail.com>
2019-09-17 20:42:59 +0200

    add and Iterate the category item component

--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ Vue.config.productionTip = false;
 Vue.component('Dropdown', Dropdown);
 Vue.component('Hero', Hero);
 
+Vue.filter('capitalize', (value) => {
+    if (value && typeof value === 'string') {
+        return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+    return '';
+});
+
 new Vue({
     router,
     store,
