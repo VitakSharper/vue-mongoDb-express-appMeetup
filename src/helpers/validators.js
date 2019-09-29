fe95c38e405c0182d3e074514771136be889ecd7
VitakSharper <v.banard@gmail.com>
2019-09-29 23:29:31 +0200

    Update validators.js

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -5,6 +5,6 @@ export const supportedFileType = (value) => {
 
     const allowedFormats = ['jpg', 'png', 'jpeg'];
     const extension = value.split('.').pop();
-    
+
     return allowedFormats.includes(extension);
 };
