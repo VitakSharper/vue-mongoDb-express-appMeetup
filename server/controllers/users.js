bc158aff300bec938a07f662af837ac0c1c4d6ec
VitakSharper <v.banard@gmail.com>
2019-09-30 22:48:02 +0200

    Add login and register to node Api

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,77 @@
 const User = require('../models/users');
+const passport = require('passport');
 
-exports.getUsers = function(req, res) {
-  User.find({})
+exports.getUsers = (req, res) => {
+    User.find({})
         .exec((errors, users) => {
 
-    if (errors) {
-      return res.status(422).send({errors});
+            if (errors) {
+                return res.status(422).send({errors});
+            }
+
+            return res.json(users);
+        });
+};
+
+exports.register = (req, res) => {
+
+    const {email, password, passwordConfirm} = req.body;
+
+    if (!email) {
+        return res.status(422).json({
+            errors: {
+                email: 'is required'
+            }
+        })
     }
 
-    return res.json(users);
-  });
-}
+    if (!password) {
+        return res.status(422).json({
+            errors: {
+                password: 'is required'
+            }
+        })
+    }
+
+    if (password !== passwordConfirm) {
+        return res.status(422).json({
+            errors: {
+                password: 'is not the same as confirmation password'
+            }
+        })
+    }
+
+    const user = new User(registerData);
+
+    return user.save((err, savedUser) => {
+        if (err) {
+            return res.status(422).json({err});
+        }
+        return res.json(savedUser);
+    });
+};
+
+exports.login = (req, res) => {
+
+    const {email, password} = req.body;
+
+    if (!email) {
+        return res.status(422).json({
+            errors: {
+                email: 'is required'
+            }
+        })
+    }
+
+    if (!password) {
+        return res.status(422).json({
+            errors: {
+                password: 'is required'
+            }
+        })
+    }
+
+    return password.authenticate('local', (err, passportUser) => {
+
+    });
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config/dev');
 
+const session = require('express-session');
+const passport = require('passport');
+const mongoDbStore = require('connect-mongodb-session')(session);
+
+const store = new mongoDbStore({
+    uri: config.DB_URI,
+    collection: 'meetupSessions'
+});
+
+store.on('err', (err) => console.log(err));
+
 require("./models/meetups");
 require("./models/users");
 require("./models/threads");
@@ -19,6 +30,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(session({
+    secret: config.SESSION_SECRET,
+    cookie: {maxAge: 360000},
+    resave: false,
+    saveUninitialized: false,
+    store
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/api/v1/meetups', meetupsRoutes);
 app.use('/api/v1/users', usersRoutes);
 app.use('/api/v1/posts', postsRoutes);
@@ -28,14 +50,14 @@ app.use('/api/v1/categories', categoriesRoutes);
 const PORT = process.env.PORT || 3001;
 
 
-(async function start() {
+(async () => {
     try {
         if (await mongoose.connect(config.DB_URI, {
             useNewUrlParser: true, useFindAndModify: false
         })) {
             console.log('DB Connected');
             app.listen(PORT, () => {
-                console.log('App is running on port: ' + PORT);
+                console.log(`App is running on port: ${PORT}`);
             });
         }
     } catch (e) {
